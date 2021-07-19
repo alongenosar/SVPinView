7550e2b3584473be12398f8f59bb1df9aa148ef2
Dubon Ya'ar <alon@bootleg.co.il>
2021-07-19 08:59:19 +0300

    Update SVPinView.swift

--- SVPinView/Source/Classes/SVPinView.swift
+++ SVPinView/Source/Classes/SVPinView.swift
@@ -102,6 +102,7 @@ public class SVPinView: UIView {
     
     private func loadView(completionHandler: (()->())? = nil) {
         let podBundle = Bundle(for: SVPinView.self)
+        print("***",podBundle)
         let nib = UINib(nibName: "SVPinView", bundle: podBundle)
         view = nib.instantiate(withOwner: self, options: nil)[0] as? UIView
         
